005d29ba5e0bc24ddc13dccfa261bf598181b7f3
tuan0306 <nguyentuan22032006@gmail.com>
2026-07-24 11:31:04 +0700

    [ADD] build docker

--- training_environment/scripts/export_to_onnx.py
+++ training_environment/scripts/export_to_onnx.py
@@ -41,9 +41,13 @@ def export_model_to_onnx():
         print("Lỗi: Không tìm thấy file trọng số.")
         return
 
+    # @tf.function(input_signature=[
+    #     tf.TensorSpec(shape=[None, max_length], dtype=tf.int32, name="encoder_input"),
+    #     tf.TensorSpec(shape=[None, max_length - 1], dtype=tf.int32, name="decoder_input")
+    # ])
     @tf.function(input_signature=[
-        tf.TensorSpec(shape=[None, max_length], dtype=tf.int32, name="encoder_input"),
-        tf.TensorSpec(shape=[None, max_length - 1], dtype=tf.int32, name="decoder_input")
+        tf.TensorSpec(shape=[None, None], dtype=tf.int32, name="encoder_input"),
+        tf.TensorSpec(shape=[None, None], dtype=tf.int32, name="decoder_input")
     ])
     def serving_fn(encoder_input,encoder_output):
         return model((encoder_input,encoder_output),training=False)
